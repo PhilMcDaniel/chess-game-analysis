25c89bd50226c2a4e3c93ceed7fac274fd1ff446
Phil McDaniel <mcdanipc@gmail.com>
2021-07-17 16:04:55 -0500

    Update game_parser.py
    Add dicts to count openings and games

--- game_parser.py
+++ game_parser.py
@@ -1,14 +1,17 @@
 import download_decompress as dd
 import os
 
-url = 'https://database.lichess.org/standard/lichess_db_standard_rated_2013-01.pgn.bz2'
-filename = 'resources/Jan2013.pgn.bz2'
+# https://database.lichess.org/
+# https://database.lichess.org/standard/lichess_db_standard_rated_2018-06.pgn.bz2
+dir = 'resources/'
+url = 'https://database.lichess.org/standard/lichess_db_standard_rated_2015-02.pgn.bz2'
+filename = dir+url[38:]
+
 
 dd.download_file(url,filename)
 dd.bz2_decompress(filename)
 
 #delete original .pgn.bz2 file
-dir = 'resources/'
 for filename in os.listdir(dir):
     if filename.endswith(".bz2"):
         fullpath = os.path.join(dir,filename) 
@@ -17,19 +20,31 @@ for filename in os.listdir(dir):
     else:
         continue
 
-#read all lines into list
-with open("resources/Jan2013.pgn",'r') as file:
-    lines = file.readlines()
-    for line in lines[:18]:
-        print(line.rstrip())
-print(len(lines))
-
+openings = dict()
+games = {'Games':0}
 #read 1 line at a time
 with open("resources/Jan2013.pgn",'r') as file:
-    for i in range(18):
+    for line in file:
         line = file.readline()
-        print(i)
-        print(line.rstrip())
+        #maintain dict with counts of each opening
+        if line[:8] == '[Opening':
+            opening = line[10:-3].rstrip()
+            #if key exists in dict, add 1 to count
+            if opening in openings:
+                openings.update({opening:openings[opening]+1})
+            else:
+            #add key to dict if it doesn't already exist
+                openings[opening]=1
+
+        #Count number of games. game line starts with "1."
+        if line[:2] == '1.':
+            games['Games'] +=1
+
+        else:
+            pass
+
+games
+openings
 
 #parse and count the openings
 
