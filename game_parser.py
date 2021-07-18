010a2a0a4aa60ab118c0cb0dddfcbd7ebc6a840e
Phil McDaniel <mcdanipc@gmail.com>
2021-07-17 21:57:12 -0500

    Update game_parser.py
    add dataframe for openings

--- game_parser.py
+++ game_parser.py
@@ -1,5 +1,6 @@
 import download_decompress as dd
 import os
+import pandas as pd
 
 # https://database.lichess.org/
 # https://database.lichess.org/standard/lichess_db_standard_rated_2018-06.pgn.bz2
@@ -30,7 +31,7 @@ with open(decomp_filename,'r') as file:
         total_lines += 1
         #maintain dict with counts of each opening
         if line[:8] == '[Opening':
-            opening = line[10:-3]
+            opening = line[10:-2]
             #if key exists in dict, add 1 to count
             if opening in openings:
                 openings.update({opening:openings[opening]+1})
@@ -45,11 +46,13 @@ with open(decomp_filename,'r') as file:
             pass
     file.close()
 
-total_lines
-games
-openings
+#total_lines
+#games
+#openings
 
 #parse and count the openings
-
+openings_df = pd.DataFrame.from_dict(openings,orient='index',columns=['Count'])
+openings_df.head(10)
+openings_df.sort_values(by='Count',ascending=False)
 #parse and count results by color
 #parse and count results by rating
