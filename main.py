2d53503208d8fe9b2da49ef39aada349abe707bf
Phil McDaniel <mcdanipc@gmail.com>
2022-06-09 14:26:32 -0500

    Update main.py
    Add process to delete split files after loading to BigQuery

--- main.py
+++ main.py
@@ -12,25 +12,25 @@ file = split.FileSplit()
 #instantiate BQ class
 bigquery = bq.BigQuery()
 
-source_file_name = 'lichess_db_standard_rated_2016-12.pgn'
-
+source_file_name = 'lichess_db_standard_rated_2017-01.pgn'
+games_file = 200000
 
 #get number of games in file
-results = file.count_games_lines(source_file_path='Downloads/{source_file_name}')
+results = file.count_games_lines(source_file_path=f'Downloads/{source_file_name}')
 results
 
 #Get number of files needed based on games, and games/file
-files = file.calc_num_files(games = results['Games'],games_per_file = 1000000)
+files = file.calc_num_files(games = results['Games'],games_per_file = games_file)
 files
 
 #create the empty shell files
-file.create_split_files(number_of_files=files,target_directory='Downloads/TMP',original_filename=source_file_name[:-4])
+file.create_split_files(number_of_files=files,target_directory=f'Downloads/TMP',original_filename=source_file_name[:-4])
 
 #get list of files in a directory with a specific file type
 file_list = file.files_in_dir(directory = '/Users/philmcdaniel/Documents/GitHub/chess-game-analysis/Downloads/TMP',file_type='pgn')
 file_list
 
-file.load_split_files(source_file = 'Downloads/{source_file_name}',target_file_list = file_list,games_per_file = 1000000)
+file.load_split_files(source_file = f'Downloads/{source_file_name}',target_file_list = file_list,games_per_file = games_file)
 
 
 #get list of files in Downloads folder
@@ -47,8 +47,13 @@ for filename in os.listdir(dir):
         #load dataframe to bigquery
         bigquery.load_df_to_BQ(dataframe)
         print(f'{filename} has been loaded to bigquery successfully')
+        
+        #remove files from TMP/DOWNLOADS after loading to BQ
+        os.remove(os.path.join(dir,filename))
     else:
         continue
+    
+
 
 #send query to bigquery, will be used for dataviz
 query = 'SELECT LEFT(game_date,7) yyyymm,COUNT(*) game_count FROM `valid-logic-327117.ChessGames.ChessGamesTable` GROUP BY 1'
