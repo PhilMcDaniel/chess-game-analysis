bd5dec3eb104b669d3bf7546a6c2208143c6df2a
Phil McDaniel <mcdanipc@gmail.com>
2023-01-07 17:36:13 -0600

    Get dag working in docker
    Get dag working in docker

--- src/lichess_rating_history.py
+++ src/lichess_rating_history.py
@@ -1,39 +1,54 @@
 import requests
 import pandas as pd
+import os
 
-usernames = ['pcmcd']
-modes = ["ultraBullet","bullet","blitz","rapid","classical","correspondence","chess960","crazyhouse","antichess","atomic","horde","kingOfTheHill","racingKings","threeCheck"]
+def write_lichess_data_to_csv():
+    usernames = ['pcmcd']
+    modes = ["ultraBullet","bullet","blitz","rapid","classical","correspondence","chess960","crazyhouse","antichess","atomic","horde","kingOfTheHill","racingKings","threeCheck"]
 
-url = 'https://lichess.org/api/user/{username}/perf/{perf}'
+    url = 'https://lichess.org/api/user/{username}/perf/{perf}'
 
-#generate url for each player
-urls = []
-for player in usernames:
-    urls.append(url.replace("{username}",player).replace("{perf}",modes[2]))
-#urls
+    #generate url for each player
+    urls = []
+    for player in usernames:
+        urls.append(url.replace("{username}",player).replace("{perf}",modes[2]))
+    #urls
 
-response = requests.get(url = urls[0])
-json_response = response.json()
+    response = requests.get(url = urls[0])
+    json_response = response.json()
 
-data = {}
-username = json_response['user']['name']
+    data = {}
+    username = json_response['user']['name']
 
-rating = json_response['perf']['glicko']['rating']
-rating_deviation = json_response['perf']['glicko']['deviation']
-rating_games_played = json_response['perf']['nb']
-recent_progress = json_response['perf']['progress']
+    rating = json_response['perf']['glicko']['rating']
+    rating_deviation = json_response['perf']['glicko']['deviation']
+    rating_games_played = json_response['perf']['nb']
+    recent_progress = json_response['perf']['progress']
 
-rating_rank = json_response['rank']
-rating_percentile = json_response['percentile']
+    rating_rank = json_response['rank']
+    rating_percentile = json_response['percentile']
 
-wins = json_response['stat']['count']['win']
-draws = json_response['stat']['count']['draw']
-losses = json_response['stat']['count']['loss']
+    wins = json_response['stat']['count']['win']
+    draws = json_response['stat']['count']['draw']
+    losses = json_response['stat']['count']['loss']
 
-data[(username,modes[2])] = {"rating":rating,"rating_deviation":rating_deviation,"rating_games_played":rating_games_played,"recent_progress":recent_progress,"rating_rank":rating_rank,"rating_percentile":rating_percentile,"wins":wins,"draws":draws,"losses":losses}
+    data[(username,modes[2])] = {"rating":rating,"rating_deviation":rating_deviation,"rating_games_played":rating_games_played,"recent_progress":recent_progress,"rating_rank":rating_rank,"rating_percentile":rating_percentile,"wins":wins,"draws":draws,"losses":losses}
 
-data[('pcmcd','blitz')]
+    #data[('pcmcd','blitz')]
 
-#dictionary to dataframe
+    #dictionary to dataframe
+    df = pd.DataFrame.from_dict(data,orient='index').reset_index()
+    df = df.rename(columns={"level_0":"username","level_1":"game_mode"})
+    #df.head()
 
-#write to csv
+    #write to csv
+
+    #create direcory if not exists
+    #TODO
+    #if file already exists, only append, else full write
+    #TODO
+
+    #df.to_csv("\lichess_rating_history.csv",index_label='row')
+    return print(df.head(5))
+
+#write_lichess_data_to_csv()
--- src/lichess_rating_history_dag.py
+++ src/lichess_rating_history_dag.py
@@ -1,6 +1,43 @@
+import airflow
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.python import PythonOperator
+from datetime import datetime,timedelta
+from airflow.utils.dates import days_ago
+
 import lichess_rating_history
 
 #docker cp {local_file} {container_id}:/{target_location}
 #docker cp lichess_rating_history_dag.py :/opt/airflow/dags/lichess_rating_history_dag.py
 #docker cp lichess_rating_history.py :/opt/airflow/dags/lichess_rating_history.py
 
+args = {
+            'owner': 'phil',    
+            #'start_date': airflow.utils.dates.days_ago(2),
+            # 'end_date': datetime(),
+            # 'depends_on_past': False,
+            'email': ['mcdanipc@gmail.com'],
+            #'email_on_failure': False,
+            #'email_on_retry': False,
+            # If a task fails, retry it once after waiting
+            # at least 5 minutes
+            #'retries': 1,
+            'retry_delay': timedelta(minutes=5),
+        }
+
+dag = DAG(
+	dag_id = "simple_python_dag",
+	default_args=args,
+	# schedule_interval='0 0 * * *',
+	schedule_interval='@once',	
+	dagrun_timeout=timedelta(minutes=60),
+	description='use case of python operator in airflow',
+	start_date=datetime(year=2022, month=2, day=1)
+    )
+
+t1 = PythonOperator(
+    task_id='task_number_1',
+    python_callable= lichess_rating_history.write_lichess_data_to_csv,
+    dag=dag,
+)
+t1
